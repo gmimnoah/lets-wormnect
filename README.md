c5dacd4aa68ef9a89ad5df432165cdb3a344673e
noah <gmimnoah@gmail.com>
2023-06-03 23:12:27 -0700

    annotated each customized theme option + wrote some more raw notes and observations in the readme

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ let's see what you can do with wormhole connect
   - `success`
   - `warning`
 * the opacity options for the `OPACITY` object imported from the `wormhole-connect` package are: `0`, `5`, `7`, then multiples of 5 from `10` to `100` ([source](https://github.com/wormhole-foundation/wormhole-connect/blob/e032790ba4496ce6068222bf89da332d3bf99610/wormhole-connect-loader/src/theme.ts#L74))
+
+### random observations
+* the `background` default `wormhole` is a full-on generated background image ([source](https://github.com/wormhole-foundation/wormhole-connect/blob/e032790ba4496ce6068222bf89da332d3bf99610/wormhole-connect/src/components/Background/BackgroundImage.tsx#L16))
+* maybe for the options under the 'Gas payment' section, the selected option should have a noticeable checkmark showing that that option is the selected one. right now, it's pretty hard to tell which option is selected + if there are even options in the first place
+* the use of the `options`'s `hover` and `select` configs are kind of inconsistent. for pop-up menus, when you hover over an item, it uses the `select` color. for the 'Gas payment' section, the selected item uses the `select` color and the item being hovered over uses the `hover` color.
+* `primary`, `secondary`, and `popover.elevation` aren't not being used (and probably should?)
+* the `divider` color is also used for the border of the search box (which it shouldn't?)
--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
 import WormholeBridge, { Theme, OPACITY, WormholeConnectConfig } from '@wormhole-foundation/wormhole-connect';
-import { lightBlue, grey, green, orange, red } from '@mui/material/colors';
+import { grey, green, orange, red, pink } from '@mui/material/colors';
 
 const customized: Theme = {
   primary: grey,
   secondary: grey,
-  divider: '#ffffff' + OPACITY[20],
-  background: {
-    default: 'wormhole',
+  divider: '#ffffff' + OPACITY[20], // color of divider for menus
+  background: { // background color of the widget, 'wormhole' is an option
+    default: '#131624', 
   },
-  text: {
-    primary: '#ffffff',
-    secondary: grey[500],
+  text: { // text colors for across the widget and other minor places
+    primary: 'white', 
+    secondary: grey[500], 
   },
-  error: red,
-  info: lightBlue,
-  success: green, 
-  warning: orange, 
-  button: {
-    primary: '#ffffff' + OPACITY[10],
-    primaryText: '#ffffff',
-    disabled: '#ffffff' + OPACITY[7],
-    disabledText: '#ffffff' + OPACITY[40],
-    action: '#ffffff',
-    actionText: '#000000',
-    hover: '#ffffff' + OPACITY[7],
+  error: red, // color of the error divs
+  info: pink, // color of the info divs
+  success: green, // color of success divs
+  warning: orange, // color of warning divs
+  button: { // colors for all the buttons
+    primary: '#ffffff' + OPACITY[10], // default button background color 
+    primaryText: '#ffffff', // default button text color
+    disabled: '#ffffff' + OPACITY[7], // disabled ... bcolor
+    disabledText: '#ffffff' + OPACITY[40], // disabled ... color
+    action: '#ffffff', // clicked ... color
+    actionText: '#000000', // clicked ... color
+    hover: '#ffffff' + OPACITY[7], // hovered ... color
   },
-  options: {
-    hover: '#ffffff' + OPACITY[7],
-    select: '#ffffff' + OPACITY[10],
+  options: { // colors for anything that involves choosing from options of things
+    hover: '#5981f3', // for hovering over the options under 'Gas payment'
+    select: '#191d28' // for hovering over any option in a pop-up modal
   },
-  card: {
-    background: '#ffffff' + OPACITY[5],
-    secondary: '#ffffff' + OPACITY[5],
-    elevation: 'none',
+  card: { // colors and float for the bodies under each 'From' and 'To' section
+    background: '#0e111b', // background color of the card
+    secondary: '#141a29', // background color of the components in the card
+    elevation: '0 0 0.25px 0.25px white', // https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
   },
-  popover: {
-    background: '#1b2033',
-    secondary: '#ffffff' + OPACITY[5],
-    elevation: 'none',
+  popover: { // colors for the menu when clicking on the wallet button when a wallet is selected
+    background: '#0e111b', // background color of the menu
+    secondary: '#ffffff' + OPACITY[5], // background color for the item being hovered over
+    elevation: '0 0 0.25px 0.25px white', // https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow (not being used rn)
   },
-  modal: {
-    background: '#0F1024',
+  modal: { // color of the area that modals do not cover on the screen
+    background: 'transparent',
   },
-  font: { // for widget-wide font typing
+  font: { // font families across the widget for header and body 
     primary: '"Inter", sans-serif',
     header: '"IBM Plex Mono", monospace',
   },
